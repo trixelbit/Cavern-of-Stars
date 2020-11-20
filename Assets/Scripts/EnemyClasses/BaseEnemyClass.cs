bd3d4f0688f173f4d67a2693d97d06ab78ce58f7
Marco Flores <marco@soulcadeinteractive.com>
2020-11-20 06:51:01 -0500

    Added an EnemyFloater Class
    + Added Enemy Floater Class that extends enemy base class

---
+++ Assets/Scripts/EnemyClasses/BaseEnemyClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyClass : MonoBehaviour
+{ 
+    private int HP = 6;
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Hit")
+        {
+            --HP;
+        }
+    }
+
+    public void CheckHP()
+    {
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/EnemyClasses/EnemyFloaterClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFloaterClass : BaseEnemyClass
+{
+    public int HP = 3;
+    public GameObject SpritePlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckHP();
+        OscilateSpritePlane();
+    }
+
+
+    private void OscilateSpritePlane()
+    { 
+        SpritePlane.transform.position = new Vector3(transform.position.x, .01f * Mathf.Sin(Time.time) + SpritePlane.transform.position.y, transform.position.z);
+    }
+
+    private void Faceplayer()
+    {
+
+    }
+
+
+
+}

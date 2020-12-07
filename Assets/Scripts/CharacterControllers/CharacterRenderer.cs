390247f9a38c8565b794ce271876812546977b06
Marco Flores <marco@soulcadeinteractive.com>
2020-12-06 20:21:07 -0500

    Level Tweeks
    + Added Invisible Walls
    + Cleaned File Structure

--- Assets/Scripts/CharacterControllers/CharacterRenderer.cs
+++ Assets/Scripts/CharacterControllers/CharacterRenderer.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class SpriteRenderer : MonoBehaviour
+public class CharacterRenderer : MonoBehaviour
 {
     public GameObject Parent;
 

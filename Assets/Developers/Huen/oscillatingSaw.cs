eb34ece62929ae852eec4744fc3022b55a83a47a
Hue-n <72900340+Hue-n@users.noreply.github.com>
2020-12-22 23:58:18 -0500

    New Scene to Prototype Saw Systems
    first commit :3

---
+++ Assets/Developers/Huen/oscillatingSaw.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oscillatingSaw : MonoBehaviour
+{
+    [Header("Saw Variables")]
+    public float speed = 3.0f;
+    public float sphereSize = 1f;
+    public Transform pointAPos, pointBPos;
+    public Transform startPos;
+    Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pointAPos.position)
+        {
+            nextPos = pointBPos.position;
+        }
+
+        if (transform.position == pointBPos.position)
+        {
+            nextPos = pointAPos.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(pointAPos.position, sphereSize);
+        Gizmos.DrawWireSphere(pointBPos.position, sphereSize);
+        Gizmos.DrawLine(pointAPos.position, pointBPos.position);
+    }
+}
---
+++ Assets/Developers/Huen/rotatingSaw.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingSaw : MonoBehaviour
+{
+    [Header("Saw Variables")]
+    public float rotateSpeed = 3.0f;
+    public float sphereSize;
+    public Transform pointOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x + rotateSpeed, 0, 0);
+        //transform.RotateAround(new Vector3(0f,0f,0f), new Vector3(1f,0f,0f),90f * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(pointOrigin.position, sphereSize);
+        Gizmos.DrawLine(transform.position, pointOrigin.position);
+    }
+}

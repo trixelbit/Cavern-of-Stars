1d01d686d0cc26821e27ee2684030b3d994f3f8e
Hue-n <72900340+Hue-n@users.noreply.github.com>
2020-12-27 00:33:23 -0500

    Created Rough Draft of Oscillating Saw
    aba

---
+++ Assets/Developers/Huen/rotatingSaw.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class rotatingSaw : MonoBehaviour
+{
+    [Header("Dependencies")]
+    public Transform origin;
+
+    [Header("Saw Properties")]
+    public float rotationSpeed = 3f;
+
+    [Header("Gizmos Properties")]
+    public float sphereSize = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector4 setColor = new Vector4(0, 0, 1, 255);
+
+        //Gizmos
+        Gizmos.color = setColor;
+        Gizmos.DrawLine(origin.position, transform.position);
+        Gizmos.DrawWireSphere(origin.position, sphereSize);
+
+        //Handles
+        Handles.color = setColor;
+        Handles.Label(origin.position, "Point A");
+    }
+}

b209fe006c1474ec5d600218269f4e1233cd9831
Hue-n <72900340+Hue-n@users.noreply.github.com>
2020-12-29 00:32:37 -0500

    Started Bullet System
    + Bullet Sphere & Light
    + Created Variables for the BulletSystem Script
    + Created a button and slider in worldspace for effective communication to the user
    
    ! Create the methods so that the bullet actually shoots
    ! Create rays so the user knows which way the bullet is travelling
    ! Try to make it so that the length of the ray is accurate to how long the bullet will be alive for (Distance)

---
+++ Assets/Developers/Huen/BulletSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSystem : MonoBehaviour
+{
+    #region Dependencies & Variables
+    //[Header("Dependencies")]
+
+    [Header("Bullet Variables")]
+    public float bulletSpeed = 3f;
+    [Range(0.5f,1)] public float bulletLife;
+
+    [Header("Gizmos and Raycast Variables")]
+    public Color setColor;
+    #endregion
+
+    #region Private Variables
+    private Vector3 bulletDir;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        
+    }
+}

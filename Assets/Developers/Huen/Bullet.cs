116403a97860635a083fdbf96e7b14ab67be7581
Hue-n <72900340+Hue-n@users.noreply.github.com>
2020-12-31 04:12:34 -0500

    Bullet Prefab
    + Bullet System implemented
    
    ! remove relationships and dependencies

---
+++ Assets/Developers/Huen/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    #region Variables
+    [Header("Bullet Variables")]
+    [Range(0,1000)] public float bulletSpeed = 5f;
+    [Range(0,5)] public float bulletLife = 1f;
+    public Vector3 bulletDir;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += bulletDir * bulletSpeed * Time.deltaTime;
+    }
+
+    public void instantiateBullet()
+    {
+        GameObject clone = Instantiate(this.gameObject, transform.position, Quaternion.Euler(0, 0, 0)); //Instantiates the bullet at the init location with no rotation
+        clone.transform.LookAt(Camera.main.transform.position, -Vector3.up);
+        Destroy(clone, bulletLife);
+    }
+}
---
+++ Assets/Developers/Huen/BulletSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BulletSpawner : MonoBehaviour
+{
+    #region Dependencies
+    [Header("Dependencies")]
+    public GameObject bullet; //references the bullet object
+    public Bullet bulletScript;
+    #endregion
+
+    #region Variables
+    [Header("Bullet Variables")]
+    public Vector3 bulletDir;
+
+    [Header("Gizmos and Raycast Variables")]
+    public Color setColor;
+    [Range(0,3)] public float gizmoSize;
+    #endregion
+
+    #region Start, Update
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { //when the spacebar is pushed
+            bulletScript.instantiateBullet();
+        }
+
+        Debug.DrawRay(transform.position, bulletDir, setColor);
+    }
+    #endregion
+}
